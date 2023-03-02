56d9a01a88601acc6b29990041def9f2ca6887f1
FaSnow <eng.fbneves@gmail.com>
2023-03-02 11:14:13 -0300

    Pasta routes criada

--- diario-eletronico/src/controllers/crudController.js
+++ diario-eletronico/src/controllers/crudController.js
@@ -1,24 +1,22 @@
 // Importar o esquema
 
+// CRUD
 class crudController {
 
+    // READ
     static read = (req, res) => {
-
     }
-
-    static readPorId = (req, res) => {
-
+    // READ-BY-ID
+    static readById = (req, res) => {
     }
+    // CREATE
     static create = (req, res) => {
-
     }
-
+    // UPDATE
     static update = (req, res) => {
-
     }
-
+    // DELETE
     static delete = (req, res) => {
-
     }
 }
 
