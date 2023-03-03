9bdcdc10dd7651de6833d98b0fe733a743cc71e4
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-03-03 17:30:04 -0300

    Add header

--- diario-eletronico/src/components/home/index.jsx
+++ diario-eletronico/src/components/home/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Cadastro from "../pages/Cadastro";
+import Header from "../header";
 
 export default props => {
     return(
         <div>
-            <h1>Home</h1>
-            <Cadastro/>
+            <Header/>
+            {/* <Cadastro/> */}
             
         </div>
     )
