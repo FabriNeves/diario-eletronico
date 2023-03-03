f32485000610e223004b2b3fc3d2554a115c3405
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-03-02 22:36:34 -0300

    Add lib Style component

--- diario-eletronico/src/App.js
+++ diario-eletronico/src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 import Home from './components/home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Home />
     </div>
   );
--- diario-eletronico/src/components/home/index.jsx
+++ diario-eletronico/src/components/home/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Cadastro from "../pages/Cadastro";
 
 export default props => {
     return(
         <div>
             <h1>Home</h1>
-            <Cadastro/>
+            
         </div>
     )
 }
