94b971e272c2e07e2674668b4d50b1429370b65a
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-03-02 23:43:01 -0300

    Add ajustes no layout

--- diario-eletronico/src/components/pages/styles.js
+++ diario-eletronico/src/components/pages/styles.js
@@ -2,6 +2,22 @@ import styled from "styled-components";
 
 
 export const Container = styled.div `
+     display: flex;
+     width: 850px;
+     border: 1px solid red;
+     background-color: aqua;
+     margin: 0 auto;
+     align-items: center;
+     justify-content: center;
+     padding: 10px;
+     
+     .box-area1 input{
 
+        margin-right: 10px;
 
+        
+
+     }
+     
+     
 `
