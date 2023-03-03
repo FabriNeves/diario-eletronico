8851aa3835900d67473cabc9f7f27e71de5213b3
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-03-02 23:13:04 -0300

    Add base do formulario.

--- diario-eletronico/src/components/pages/Cadastro.jsx
+++ diario-eletronico/src/components/pages/Cadastro.jsx
@@ -1,10 +1,35 @@
 
 
-export default props =>{
-    return(
+export default props => {
+    return (
         <form action="">
-
-          
+            <div>
+                <label>Nome:</label> <br />
+                <input type="text" />
+                <select name="" id="">
+                    <option value="">Sexo:</option>
+                    <option value="">Maculino</option>
+                    <option value="">Feminino</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="">Data de Nascimento:</label><br/>
+                <input type="text" /><br/>
+                <label htmlFor="">Idade</label><br/>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Disciplina:</label><br/>
+                <input type="text" /> 
+                <br />
+                <label htmlFor="">Nota:</label>
+                <br />
+                <input type="text" />
+                <br />
+                <label htmlFor="">Situação Final:</label><br />
+                <input type="text" />
+                
+            </div>
         </form>
     )
 }
